0b618e67745d164d4e8bbf542a26cb6791095ab7
Won Gayeon <57881683+silano08@users.noreply.github.com>
2024-04-03 19:30:34 +0900

    테스트코드 주석 추가

--- src/test/java/com/example/synchronizestudy/facade/BalanceFacadeTest.java
+++ src/test/java/com/example/synchronizestudy/facade/BalanceFacadeTest.java
@@ -27,10 +27,20 @@ class BalanceFacadeTest {
     }
 
     @Test
-    void increase() {
+    void increaseSameTime() {
+        // 잔고 입금 요청이 동시에 2개 이상 올 경우 실패해야함
     }
 
     @Test
-    void decrease() {
+    void decreaseSameTime() {
+        //  잔고 출금 요청이 동시에 2개 이상 올 경우 차례대로 실행
+
+    }
+
+    @Test
+    void increaseAndDecreaseSameTime() {
+        //  잔고 입금과 출금 요청은 동시에 올 수 있고, 요청 온 차례대로 실행
+        // 입금->출금 , 출금->입금으로 로직 작성할 것
+
     }
 }
