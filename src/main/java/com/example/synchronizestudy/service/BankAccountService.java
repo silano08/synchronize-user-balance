7b779531f182a503188e439e319ac6b4140b15f5
Won Gayeon <57881683+silano08@users.noreply.github.com>
2024-04-03 19:20:18 +0900

    입금/출금 요청이 동시에 온 경우를 커버하기 위해 ReentrantLock사용

--- src/main/java/com/example/synchronizestudy/service/BankAccountService.java
+++ src/main/java/com/example/synchronizestudy/service/BankAccountService.java
@@ -5,10 +5,15 @@ import com.example.synchronizestudy.repository.BankAccountRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.OptimisticLockException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Service
 public class BankAccountService {
 
     private BankAccountRepository bankAccountRepository;
+    private final Lock lock = new ReentrantLock(); // 어플리케이션 레벨에서 동기화를 구현할때 사용
 
     public BankAccountService(BankAccountRepository bankAccountRepository) {
         this.bankAccountRepository = bankAccountRepository;
@@ -16,7 +21,6 @@ public class BankAccountService {
 
     public Long getBalance(Long id){
         // 잔고 내역 조회
-
         BankAccount balance = bankAccountRepository.findByIdWithOptimisticLock(id);
         return balance.getbalance();
     }
@@ -26,22 +30,29 @@ public class BankAccountService {
         // 잔고 입금
         // 요청이 동시에 2개이상 올 경우 실패 -> 실패했을때 실패라고 출력,옵티미스틱 락 사용
         // 출금과 입금요청이 동시에오면 요청온 차례대로 실행
-
-        BankAccount balance = bankAccountRepository.findByIdWithOptimisticLock(id);
-        balance.decrease(quantity);
-
-        bankAccountRepository.saveAndFlush(balance);
+        lock.lock();
+        try {
+            BankAccount balance = bankAccountRepository.findByIdWithOptimisticLock(id);
+            balance.increase(quantity);
+            bankAccountRepository.saveAndFlush(balance);
+        } catch (OptimisticLockException e) {
+            throw new RuntimeException("동시에 여러 입금 요청이 불가능합니다.");
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Transactional
     public void decrease(Long id, Long quantity){
         // 잔고 출금
         // 요청이 동시에 2개이상 올 경우 차례대로 실행 -> 실패했을때 재시도하도록 옵티미스틱 락 사용
-
-        BankAccount balance = bankAccountRepository.findByIdWithOptimisticLock(id);
-        balance.increase(quantity);
-
-        bankAccountRepository.saveAndFlush(balance);
-
+        lock.lock();
+        try {
+            BankAccount balance = bankAccountRepository.findByIdWithOptimisticLock(id);
+            balance.decrease(quantity);
+            bankAccountRepository.saveAndFlush(balance);
+        } finally {
+            lock.unlock();
+        }
     }
 }
